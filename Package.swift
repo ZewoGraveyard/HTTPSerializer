f0e2497849eb10ab8ddc18fcbf16c2d7a48c4a03
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-24 22:34:03 -0300

    it starts

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+	name: "HTTSerializer",
+	dependencies: [
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/swiftx/s4.git", majorVersion: 0, minor: 1)
+    ]
+)
