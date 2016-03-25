ae20205a6d32706cfc13c88a466741b526e481e8
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-24 22:46:33 -0300

    fix typo

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "HTTSerializer",
+	name: "HTTPSerializer",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/swiftx/s4.git", majorVersion: 0, minor: 1)
