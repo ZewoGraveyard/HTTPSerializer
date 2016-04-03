cd4a8fe8548e364ab2c613f2b8463c55e0777b7d
Scott Byrns <contact@scottbyrns.com>
2016-04-03 17:28:21 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 let package = Package(
 	name: "HTTPSerializer",
 	dependencies: [
-        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/SwiftX/S4.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/scottbyrns/Data.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/scottbyrns/S4.git", majorVersion: 0, minor: 2),
     ]
 )
