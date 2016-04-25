cf06eed71b0e50e0a4a599e9c6467a736d53ba90
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-24 23:05:15 -0300

    add tests

---
+++ Tests/HTTPSerializer/HTTPSerializerTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import HTTPSerializer
+
+class HTTPSerializerTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension HTTPSerializerTests {
+    static var allTests : [(String, HTTPSerializerTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
