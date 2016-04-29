36ad125d24a5418fb13fd30c85287ef65c642e1d
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-04-29 23:35:57 +0200

    Updated Swift 04-25

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "HTTPSerializer",
 	dependencies: [
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 5),
     ]
 )
--- README.md
+++ README.md
@@ -41,7 +41,7 @@ import PackageDescription
 
 let package = Package(
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 5)
+		.Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 6)
 	]
 )
 ```
