00cf1a8a4b992a909fe4d314eb570a78113067c6
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 14:57:24 +0900

    Updated for 05-03 Snapshot

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "HTTPSerializer",
 	dependencies: [
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6),
     ]
 )
