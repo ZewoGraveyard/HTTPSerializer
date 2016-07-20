f88c4c9b741aeae7fa9b0f0da12e7bd1fee5ca56
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-20 08:48:29 +0200

    Updated Swift and dependencies, needs adapting...

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "HTTPSerializer",
 	dependencies: [
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 8)
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 9)
     ]
 )
